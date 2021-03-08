70a0377e22e100de88cbeb4e240fb50a1e332bfd
Orazio-svn <orazio.ishm@gmail.com>
2021-03-08 19:39:27 +0100

    Create CrossoutMarketBotApplicationTests.java

---
+++ src/test/java/fr/alexpado/bots/cmb/CrossoutMarketBotApplicationTests.java
@@ -0,0 +1,12 @@
+package fr.alexpado.bots.cmb;
+
+import org.junit.jupiter.api.Test;
+
+class CrossoutMarketBotApplicationTests {
+
+    @Test
+    public void testIsUndefined() {
+        // TODO : Write some tests someday, I really need to learn these...
+    }
+
+}
